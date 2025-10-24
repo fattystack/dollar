815669b22b3fedd15d73a97660533d1cdcc8ccf3
Eric Qiu <ericruiqiu@gmail.com>
2025-10-23 20:18:46 -0400

    temp

--- app/src/main/java/com/fatchoy/dollar/ui/components/budgetProgressBarModelMaker.kt
+++ app/src/main/java/com/fatchoy/dollar/ui/components/budgetProgressBarModelMaker.kt
@@ -1,26 +1,35 @@
 package com.fatchoy.dollar.ui.components
 
+import android.graphics.drawable.Icon
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
 internal fun BudgetProgressBarModelMaker (
+    modifier: Modifier = Modifier.size(48.dp),
     progress: Float,
     trackColor: Color = Color(0XFF9B9B9B),
+    icon: ImageVector = Icons.Default.Check,
+    iconScale: Float = 0.5f,
     strokeWidth: Dp = 20.dp,
+    contentDescription: String? = null
 ) {
 
     val p by animateFloatAsState(progress.coerceIn(0f,1f), label = "ring")
@@ -37,6 +46,14 @@ internal fun BudgetProgressBarModelMaker (
            modifier = Modifier.fillMaxSize(),
            trackColor = trackColor,
        )
+       Icon (
+           imageVector = icon,
+           contentDescription = contentDescription,
+           modifier = Modifier.size((modifier as? Modifier)?.let {
+               24.dp
+           } ?: (48.dp * iconScale))
+       )
+
    }
 
 
