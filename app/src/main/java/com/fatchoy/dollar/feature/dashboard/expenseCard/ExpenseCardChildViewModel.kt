4046fb617f43423f79e1fa05639332cca96ff44b
Eric Qiu <ericruiqiu@gmail.com>
2025-10-26 15:37:14 -0400

    changes

--- app/src/main/java/com/fatchoy/dollar/feature/dashboard/expenseCard/ExpenseCardChildViewModel.kt
+++ app/src/main/java/com/fatchoy/dollar/feature/dashboard/expenseCard/ExpenseCardChildViewModel.kt
@@ -1,8 +1,19 @@
 package com.fatchoy.dollar.feature.dashboard.expenseCard
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 internal class ExpenseCardChildViewModel(
     initialState: ExpenseCardViewState
 ) : ViewModel() {
+
+    private val _state = MutableStateFlow(recomputeExpenses(initialState))
+
+    val state = StateFlow<ExpenseCardViewState>
+
+
+    private fun recomputeExpenses(s: ExpenseCardViewState) {
+        val
+    }
 }
