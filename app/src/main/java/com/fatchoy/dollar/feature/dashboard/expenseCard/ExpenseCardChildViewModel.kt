00db4e984bf574142cef1feeb5fa14d313f4dc4d
Eric Qiu <ericruiqiu@gmail.com>
2025-10-26 15:03:47 -0400

    initial

---
+++ app/src/main/java/com/fatchoy/dollar/feature/dashboard/expenseCard/ExpenseCardChildViewModel.kt
@@ -0,0 +1,8 @@
+package com.fatchoy.dollar.feature.dashboard.expenseCard
+
+import androidx.lifecycle.ViewModel
+
+internal class ExpenseCardChildViewModel(
+    initialState: ExpenseCardViewState
+) : ViewModel() {
+}
